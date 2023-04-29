c776af44824a0cf57954a250fc44a9b7f9a3b530
sharman <sharmandebnath@gmail.com>
2023-04-29 21:36:38 +0530

    ga class code

---
+++ index.js
@@ -0,0 +1,13 @@
+const str = "hello"
+
+console.log(str)
+
+const str2 = "world"
+
+console.log(str + " " + str2)
+
+if (str.length > str2.length) {
+    console.log("trueee")
+} else {
+    console.log("false....")
+}
